0c832108d26faca39e055384dadc2bd294c04481
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-03-24 09:45:00 +0530

    check balance

---
+++ generate-keypair/check-balance.ts
@@ -0,0 +1,16 @@
+import { Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+const publicKey = new PublicKey("EYw7EVxT1yrCAzSWHc6pr9mZfCjKFU9i4m2buAecVQQT") //public key of the account
+const lenpub = publicKey.toBase58().length; 
+// to handle invalid wallet addresses
+if(lenpub!==44 || !publicKey){
+    console.log("Invalid public key");}
+
+//connecting to devnet
+// let connection = new Connection("https://api.devnet.solana.com","confirmed");
+
+//connecting to mainnet
+const connection = new Connection("https://api.mainnet-beta.solana.com","confirmed");
+const balanceinlampots = await connection.getBalance(publicKey);
+const balance = balanceinlampots/LAMPORTS_PER_SOL;
+console.log(`
+    balance for account ${publicKey} is ${balance} SOL`)
