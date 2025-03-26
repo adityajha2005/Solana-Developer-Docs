d47526de8e90c0c702fdde6ef5b26475aa6646a0
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-03-26 10:16:50 +0530

    adapter issue fixed

--- solana-ping-frontend/components/WalletContextProvider.tsx
+++ solana-ping-frontend/components/WalletContextProvider.tsx
@@ -7,10 +7,14 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
 require("@solana/wallet-adapter-react-ui/styles.css");
+import { PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
+
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const endpoint = clusterApiUrl("devnet");
-  const wallets = useMemo(() => [], []);
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new TorusWalletAdapter()],
+    []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
