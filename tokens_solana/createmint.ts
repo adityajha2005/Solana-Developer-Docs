f6251f3731c07a0da39045fc95f1c8d2b6681379
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-03-28 09:32:42 +0530

    module2 initialized

---
+++ tokens_solana/createmint.ts
@@ -0,0 +1,35 @@
+import * as web3 from "@solana/web3.js"
+import * as token from "@solana/spl-token"
+
+//spl token mint (token creator)
+async function buildCreateMintTransaction(
+    connection: web3.Connection,
+    payer: web3.PublicKey,
+    decimals: number,
+    ): Promise<web3.Transaction> {
+        const lamports = await token.getMinimumBalanceForRentExemptMint(connection);
+        const accountKeypair = web3.Keypair.generate();
+        const programId = token.TOKEN_PROGRAM_ID;
+
+        const transaction = new web3.Transaction().add(
+            //creating new account for mint
+            web3.SystemProgram.createAccount({
+                fromPubkey: payer,
+                newAccountPubkey : accountKeypair.publicKey,
+                space : token.MINT_SIZE, //allocating space for minting
+                lamports,
+                programId,
+            }),
+            //sets up mint authority i.e who can create new tokens
+            //sets the freeze authority i.e who can freeze the token
+            token.createInitializeMintInstruction(
+                accountKeypair.publicKey,
+                decimals,
+                payer,
+                payer,
+                programId,
+            ),
+        );
+        return transaction;
+
+    }
